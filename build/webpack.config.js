6a0c012ebc22d794729ed116a723c06e73ab892c
jiachengnlong <jiachenglong@ybm100.com>
2021-07-28 14:23:21 +0800

    webpackdemo

---
+++ build/webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const path_url = '../'
+console.log(1);
+module.exports = {
+  // JS 执行入口文件
+  entry: path_url + 'main.js',
+  output: {
+    // 把所有依赖的模块合并输出到一个 bundle.js 文件
+    filename: 'bundle.js',
+    // 输出文件都放到 dist 目录下
+    path: path.resolve(__dirname, path_url + 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        // 用正则去匹配要用该 loader 转换的 css 文件
+        test: /\.css$/,
+        loaders: ['style-loader', 'css-loader'],
+      }
+    ]
+  }
+};
