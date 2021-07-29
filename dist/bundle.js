3a0ed19164daff02e173b9aaa642f16e23aaaee2
zalss <zalss@zalssdeMacBook-Pro.local>
2021-07-29 09:55:44 +0800

    输出一致

--- dist/bundle.js
+++ dist/bundle.js
@@ -96,8 +96,8 @@ if(content.locals) module.exports = content.locals;
 if(false) {
 	// When the styles change, update the <style> tags
 	if(!content.locals) {
-		module.hot.accept("!!./node_modules/_css-loader@0.28.11@css-loader/index.js!./main.css", function() {
-			var newContent = require("!!./node_modules/_css-loader@0.28.11@css-loader/index.js!./main.css");
+		module.hot.accept("!!./node_modules/css-loader/index.js!./main.css", function() {
+			var newContent = require("!!./node_modules/css-loader/index.js!./main.css");
 			if(typeof newContent === 'string') newContent = [[module.id, newContent, '']];
 			update(newContent);
 		});
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,22 @@
-const path = require('path');
-const path_url = './'
+const path = require("path");
+const path_url = "./";
 console.log(1);
 module.exports = {
   // JS 执行入口文件
-  entry: path_url + 'main.js',
+  entry: path.resolve(__dirname, "./main.js"),
   output: {
     // 把所有依赖的模块合并输出到一个 bundle.js 文件
-    filename: 'bundle.js',
+    filename: "bundle.js",
     // 输出文件都放到 dist 目录下
-    path: path.resolve(__dirname, path_url + 'dist'),
+    path: path.resolve(__dirname, "./dist"),
   },
   module: {
     rules: [
       {
         // 用正则去匹配要用该 loader 转换的 css 文件
         test: /\.css$/,
-        loaders: ['style-loader', 'css-loader'],
-      }
-    ]
-  }
+        loaders: ["style-loader", "css-loader"],
+      },
+    ],
+  },
 };
