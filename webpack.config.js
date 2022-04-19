9f5b27f62e0c14d6b495f537c24a14fde2149644
jiachengnlong <251895921@qq.com>
2022-04-19 11:15:55 +0800

    plugins, loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,12 @@
 const path = require('path');
+const ExtractTextPlugin  = require("extract-text-webpack-plugin")
 const path_url = './'
 console.log(1);
 module.exports = {
   // JS 执行入口文件
-  entry: path_url + 'main.js',
+  entry: {
+    main: path_url + 'main.js',
+  },
   output: {
     // 把所有依赖的模块合并输出到一个 bundle.js 文件
     filename: 'bundle.js',
@@ -15,10 +18,18 @@ module.exports = {
       {
         // 用正则去匹配要用该 loader 转换的 css 文件
         test: /\.css$/,
-        loaders: ['style-loader', 'css-loader'],
-      }
+        use: ExtractTextPlugin.extract({
+          use: ['css-loader'],
+        })
+      },
     ]
   },
+  plugins: [
+    // 把注入到 bundle.js 文件里的 CSS 提取到单独的文件中
+    new ExtractTextPlugin  ({
+      filename: `a.css`,
+    }),
+  ],
   devServer:{
     headers: { // response headers
       'X-foo':'jcl' 
